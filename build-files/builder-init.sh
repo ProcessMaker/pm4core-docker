d9962ef85968df1836ae9bff99c45d2b74ae161b
Nolan Ehrstrom <github@nolanpro.com>
2021-12-16 14:18:25 -0800

    Specify path

--- build-files/builder-init.sh
+++ build-files/builder-init.sh
@@ -7,7 +7,8 @@ done
 
 composer global config repositories.pm4-tools vcs https://${GITHUB_TOKEN}@github.com/ProcessMaker/pm4-tools.git
 composer global require processmaker/pm4-tools:dev-master
-pm install-packages-ci
+
+/root/.config/composer/vendor/bin install-packages-ci
 
 touch /code/pm4/done.txt
 
