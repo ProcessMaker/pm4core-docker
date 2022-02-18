b8a883b340aeee6d278ccb8f250ce96a6962e8b7
Nolan Ehrstrom <github@nolanpro.com>
2022-02-18 15:03:01 -0800

    Use image tag env var

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ -z "${IMAGE_TAG}" ]; then
-  TAG="latest"
-else
-  TAG=$(echo "${IMAGE_TAG}" | sed 's;/;-;g')
-fi
-
-docker commit $(./container.sh) processmaker/pm4app:"$TAG"
+docker commit $(./container.sh) processmaker/pm4app:"$IMAGE_TAG"
 echo "$DOCKERHUB_TOKEN" | docker login -u processmaker --password-stdin
-docker push processmaker/pm4app:"$TAG"
+docker push processmaker/pm4app:"$IMAGE_TAG"
